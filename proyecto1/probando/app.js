3485c128957937bee3e2a86518796f76f68879f3
MrKetchupp <6w00ywin@etereo.anonaddy.me>
2025-03-01 23:36:59 -0600

    nuevos cambios en app.js

---
+++ proyecto1/probando/app.js
@@ -0,0 +1,10 @@
+let API_KEY = "oZoFr87wYGCzaw5FY1Yh9CtIDDtQY9Tt2hVZSolqaagNolIx8y5YeTqMhAN9FqYl";
+
+let port = 3000;
+let debug = true;
+
+console.log(`API_KEY: ${API_KEY}`);
+console.log(`Servidor corriendo en puerto ${port}`);
+if (debug) {
+    console.log('Modo depuración activado');
+}
