ee88500a8b2bdf445201be2e90d13efe17d9d351
MrKetchupp <6w00ywin@etereo.anonaddy.me>
2025-03-01 23:44:14 -0600

    Cambios en app.js

--- proyecto1/probando/app.js
+++ proyecto1/probando/app.js
@@ -1,9 +1,11 @@
-let API_KEY = "oZoFr87wYGCzaw5FY1Yh9CtIDDtQY9Tt2hVZSolqaagNolIx8y5YeTqMhAN9FqYl";
+// Cargar variables del archivo .env
+require('dotenv').config();
 
+const apiKey = process.env.API_KEY;
 let port = 3000;
 let debug = true;
 
-console.log(`API_KEY: ${API_KEY}`);
+console.log(`apiKey: ${apiKey}`);
 console.log(`Servidor corriendo en puerto ${port}`);
 if (debug) {
     console.log('Modo depuración activado');
